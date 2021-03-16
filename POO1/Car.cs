6131ec95b7b9d985d797573ff9bf6c39e200fbe2
kevnefeg <62613609+kevnefeg@users.noreply.github.com>
2021-03-16 17:57:37 -0600

    add

--- POO1/Car.cs
+++ POO1/Car.cs
@@ -2,6 +2,8 @@ using System;
 
 public class Car
 {
+     int gallons =0;
+     int kmm =0;
     //attributes
     private string color;
     private string brand;
@@ -21,7 +23,7 @@ public class Car
 
     public void fullgastank()
     {
-        int gallons =0;
+       
         Console.Write("How many Gallons would you like? ");
         gallons = Int32.Parse(Console.ReadLine());
 
@@ -29,11 +31,21 @@ public class Car
 
     public void km()
     {
+        
+        Console.Write("How many Km had you traveled? ");
+        kmm = Int32.Parse(Console.ReadLine());
 
     }
 
     public void remaininggasoline()
     {
+        double remaininggasoline = 0;
+        double tankreamining =0;
+        remaininggasoline = kmm*0.25;
+        tankreamining = gallons-remaininggasoline;
+        Console.Write("Your reamining gasoline is : " + tankreamining);
+        
+
 
     }
 }
--- POO1/Program.cs
+++ POO1/Program.cs
@@ -9,11 +9,11 @@ namespace POO1
             
             Console.WriteLine("Car #1 ");
             Car car01 = 
-            new Car("Rojo","Toyota ");
+            new Car("Red","Toyota ");
             car01.fullgastank();
             car01.km();
             car01.remaininggasoline();
-            
+
 
         }
     }
